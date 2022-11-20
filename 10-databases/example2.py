0c2072c3755c1f0b27c3ebdad1b33d872531fa9b
ivngmz2018 <ivngmz2018@gmail.com>
2022-11-20 18:10:22 +0100

    update: new snippets

--- 10-databases/example2.py
+++ 10-databases/example2.py
@@ -13,14 +13,14 @@ import psycopg2
 
 
 def create_table():
-    conn=psycopg2.connect("dbname='store' user='postgres' password='Zaragoza2020' host='192.168.0.245' port='5432'")
+    conn=psycopg2.connect("dbname='store' user='postgres' password='Password' host='192.168.0.245' port='5432'")
     cur=conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     conn.commit()
     conn.close()
 
 def insert(item,quantity,price):
-    conn=psycopg2.connect("dbname='store' user='postgres' password='Zaragoza2020' host='192.168.0.245' port='5432'")
+    conn=psycopg2.connect("dbname='store' user='postgres' password='Password' host='192.168.0.245' port='5432'")
     cur=conn.cursor()
     # cur.execute("INSERT INTO store VALUES ('%s','%s','%s')" % (item,quantity,price))
     cur.execute("INSERT INTO store VALUES (%s,%s,%s)", (item,quantity,price))
@@ -28,7 +28,7 @@ def insert(item,quantity,price):
     conn.close()
 
 def view():
-    conn=psycopg2.connect("dbname='store' user='postgres' password='Zaragoza2020' host='192.168.0.245' port='5432'")
+    conn=psycopg2.connect("dbname='store' user='postgres' password='Password' host='192.168.0.245' port='5432'")
     cur=conn.cursor()
     cur.execute("SELECT * FROM store")
     rows=cur.fetchall()
@@ -36,21 +36,21 @@ def view():
     return rows
 
 def delete(item):
-    conn=psycopg2.connect("dbname='store' user='postgres' password='Zaragoza2020' host='192.168.0.245' port='5432'")
+    conn=psycopg2.connect("dbname='store' user='postgres' password='Password' host='192.168.0.245' port='5432'")
     cur=conn.cursor()
     cur.execute("DELETE FROM store WHERE item=%s",(item,))
     conn.commit()
     conn.close()
 
 def update(quantity,price,item):
-    conn=psycopg2.connect("dbname='store' user='postgres' password='Zaragoza2020' host='192.168.0.245' port='5432'")
+    conn=psycopg2.connect("dbname='store' user='postgres' password='Password' host='192.168.0.245' port='5432'")
     cur=conn.cursor()
     cur.execute("UPDATE store SET quantity=%s,price=%s WHERE item=%s",(quantity,price,item))
     conn.commit()
     conn.close()
 
 def view2():
-    conn=psycopg2.connect("dbname='store' user='postgres' password='Zaragoza2020' host='192.168.0.245' port='5432'")
+    conn=psycopg2.connect("dbname='store' user='postgres' password='Password' host='192.168.0.245' port='5432'")
     cur=conn.cursor()
     cur.execute("select count(item) from store where item = 'Beer'")
     rows=cur.fetchall()
